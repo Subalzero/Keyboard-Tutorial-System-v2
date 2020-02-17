d0273525375d05391e6dad8f5be8ebc05db46fb5
Lord Alzer <alzercasino@gmail.com>
2020-02-17 17:03:26 +0800

    Created UserList

---
+++ Keyboard Tutorial System/UserList.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include "UserList.h"
+
+rescan::UserList::UserList()
+{
+}
+
+rescan::UserList::~UserList()
+{
+}
+
+void rescan::UserList::AddEmptyUser()
+{
+}
+
+void rescan::UserList::AddUserScore(BestScore score, int userID)
+{
+}
+
+void rescan::UserList::RemoveUser(int userID)
+{
+}
